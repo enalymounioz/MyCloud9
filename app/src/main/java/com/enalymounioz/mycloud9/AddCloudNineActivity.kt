6c3598a6fd2e88bfd42f0a093d13c549244067fc
Eleni Voreopoulou <evoreopoulou@wings-ict-solution.eu>
2022-12-06 11:51:53 +0200

    Initial commit

---
+++ app/src/main/java/com/enalymounioz/mycloud9/AddCloudNineActivity.kt
@@ -0,0 +1,11 @@
+package com.enalymounioz.mycloud9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCloudNineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_cloud_nine)
+    }
+}
